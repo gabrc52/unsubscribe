8d189b1bf7851444f15ead7b03226b4199138e1d
Anna Kwon <annakwon@mit.edu>
2024-01-24 04:04:58 -0500

    single comment

--- client/src/components/modules/SingleComment.js
+++ client/src/components/modules/SingleComment.js
@@ -13,9 +13,7 @@ import { Link } from "react-router-dom";
 const SingleComment = (props) => {
   return (
     <div className="Card-commentBody">
-      <Link to={`/profile/${props.creator_googleid}`} className="u-link u-bold">
-        {props.creator_name}
-      </Link>
+      <p>{props.creator_googleid}</p>
       <span>{" | " + props.content}</span>
     </div>
   );
