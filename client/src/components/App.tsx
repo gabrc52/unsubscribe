b6dba4e25be885235c199e8c088f5b4d91f222bd
Ari <ariapero@mit.edu>
2024-01-23 20:15:24 -0500

    YourPosts/Profile Page Support

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,12 +4,15 @@ import { CredentialResponse } from "@react-oauth/google";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { get, post } from "../utilities";
+import NavBar from "./modules/NavBar.js";
 import NotFound from "./pages/NotFound";
 import Feed from "./pages/Feed.js";
+import YourPosts from "./pages/YourPosts";
 // import Skeleton from "./pages/Skeleton";
 import { socket } from "../client-socket";
 import User from "../../../shared/User";
 import "../utilities.css";
+import "./App.css";
 
 const App = () => {
   const [userId, setUserId] = useState<string | undefined>(undefined);
@@ -47,18 +50,26 @@ const App = () => {
   // NOTE:
   // All the pages need to have the props extended via RouteComponentProps for @reach/router to work properly. Please use the Skeleton as an example.
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          element={
-            <Feed handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
-            // <Skeleton handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
-          }
-          path="/"
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <NavBar />
+      {/* <NavBar handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} /> */}
+      <div className="App-container">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                // <Feed />
+                <Feed handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
+                // <Skeleton handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
+              }
+              path="/"
+            />
+            <Route path="/profile/:userId" element={<YourPosts />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 };
 
---
+++ client/src/components/pages/YourPosts.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { get } from "../../utilities";
+
+import "../../utilities.css";
+import "./YourPosts.css";
+
+// TO DO: IMPORT POSTS FROM FEED OR DATABASE THAT BELONG TO USER
+
+const YourPosts = (props) => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    document.title = "Your Posts (and Profile)";
+    get(`/api/user`, { userid: props.userId }).then((userObj) => setUser(userObj));
+  }, []);
+
+  if (!user) {
+    return <div> Loading! </div>;
+  }
+  return (
+    <>
+      <div className="YourPosts-avatarContainer">
+        <div className="YourPosts-avatar" /> {/* should be google/mit avatar or upload own */}
+      </div>
+      <h1 className="YourPosts-name u-textCenter">{user.name}</h1>{" "}
+      {/* should be google/mit name, do not allow changes */}
+      <hr className="YourPosts-linejj" />
+      <div className="u-flex">
+        <div className="YourPosts-subContainer u-textCenter">
+          <h4 className="YourPosts-subTitle">About Me</h4>
+          <div id="profile-description">
+            I am really allergic to cats i don't know why i have a catbook
+          </div>
+        </div>
+        <div className="YourPosts-subContainer u-textCenter">
+          <h4 className="YourPosts-subTitle">My Favorite Type of Cat</h4>
+          <div id="favorite-cat">corgi</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default YourPosts;
