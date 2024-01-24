31a3be44aebfe1569ba58c742a3f857cec2e78a3
Ari <ariapero@mit.edu>
2024-01-23 20:39:25 -0500

    SingleComment module added

---
+++ client/src/components/modules/SingleComment.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+/**
+ * Component to render a single comment
+ *
+ * Proptypes
+ * @param {string} _id of comment
+ * param {string} creator_name
+ * @param {string} creator_id
+ * @param {string} content of the comment
+ */
+const SingleComment = (props) => {
+  return (
+    <div className="Card-commentBody">
+      <Link to={`/profile/${props.creator_id}`} className="u-link u-bold">
+        {/* {props.creator_name} */}
+        {/* props.creator_id.{RETRIEVE NAME FROM USER.G_ID} */}
+      </Link>
+      <span>{" | " + props.content}</span>
+    </div>
+  );
+};
+
+export default SingleComment;
