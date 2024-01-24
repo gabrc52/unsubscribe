6f840384fb8d5793ef28376694e8b24759a87f85
Anna Kwon <annakwon@mit.edu>
2024-01-24 04:03:53 -0500

    new post input

--- client/src/components/modules/NewPostInput.js
+++ client/src/components/modules/NewPostInput.js
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
-
 import "./NewPostInput.css";
-// import { post } from "../../../utilities";
+import { post } from "../../utilities";
 
-/**
- * create new food event
- * TODO: handle API
- * Proptypes
- * @param {({foodType, content, photo}) => void} onSubmit
- */
 const NewPostInput = (props) => {
-  const [foodType, setFoodType] = useState("");
+  const [food_type, setFoodType] = useState("");
+  const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [photo, setPhoto] = useState("");
+  const [photos, setPhotos] = useState([]);
 
   const handleFoodTypeChange = (event) => {
     setFoodType(event.target.value);
+  };
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
   };
 
   const handleContentChange = (event) => {
@@ -23,49 +21,79 @@ const NewPostInput = (props) => {
   };
 
   const handlePhotoChange = (event) => {
-    setPhoto(event.target.value);
+    setPhotos([event.target.value]);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    props.onSubmit && props.onSubmit({ foodType, content, photo });
+    props.onSubmit && props.onSubmit({ food_type, title, content, photos });
     setFoodType("");
+    setTitle("");
     setContent("");
-    setPhoto("");
+    setPhotos([]);
   };
 
   return (
-    <div className="NewPostInput">
-      <input>
-        Food Type:
-        <input
-          type="text"
-          value={foodType}
-          onChange={handleFoodTypeChange}
-          className="NewPostInput-input"
-        />
-      </input>
-
-      <input>
-        Content:
-        <textarea value={content} onChange={handleContentChange} className="NewPostInput-input" />
-      </input>
-
-      <input>
-        Photo URL:
-        <input
-          type="text"
-          value={photo}
-          onChange={handlePhotoChange}
-          className="NewPostInput-input"
-        />
-      </input>
-
-      <button type="submit" className="NewPostInput-button u-pointer" onClick={handleSubmit}>
+    <div className="u-flex">
+      <input
+        type="text"
+        placeholder="Food Type"
+        value={food_type}
+        onChange={handleFoodTypeChange}
+        className="NewPostInput-input"
+      />
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={handleTitleChange}
+        className="NewPostInput-input"
+      />
+      <textarea
+        placeholder={props.defaultText}
+        value={content}
+        onChange={handleContentChange}
+        className="NewPostInput-input"
+      />
+      <input
+        type="text"
+        placeholder="Photo URL"
+        value={photos[0] || ""}
+        onChange={handlePhotoChange}
+        className="NewPostInput-input"
+      />
+      <button
+        type="submit"
+        className="NewPostInput-button u-pointer"
+        value="Submit"
+        onClick={handleSubmit}
+      >
         Submit
       </button>
     </div>
   );
 };
 
-export default NewPostInput;
+const NewComment = (props) => {
+  const addComment = ({ food_type, title, content, photos }) => {
+    const body = { parent: props.foodeventId, food_type, title, content, photos };
+    post("/api/comment", body).then((comment) => {
+      props.addNewComment(comment);
+    });
+  };
+
+  return <NewPostInput defaultText="New Comment" onSubmit={addComment} />;
+};
+
+const NewFoodevent = (props) => {
+  const addFoodevent = ({ food_type, title, content, photos }) => {
+    const body = { food_type, title, content, photos };
+    post("/api/foodevent", body).then((foodevent) => {
+      props.addNewFoodevent(foodevent);
+    });
+  };
+
+  return <NewPostInput defaultText="New Foodevent" onSubmit={addFoodevent} />;
+};
+
+export { NewComment, NewFoodevent };
