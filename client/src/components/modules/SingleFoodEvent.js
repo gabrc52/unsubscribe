db5e4e81b88cb9765e3514d374fe3642dac9456e
Anna Kwon <annakwon@mit.edu>
2024-01-24 21:51:14 -0500

    single food event

--- client/src/components/modules/SingleFoodEvent.js
+++ client/src/components/modules/SingleFoodEvent.js
@@ -8,14 +8,15 @@ import { Link } from "react-router-dom";
  * @param {string} _id of the food post
  * @param {string} creator_googleid
  * @param {string} content of the story
- * @param {string} photo of food
+ * @param {string} photos of food
  * @param {string} food_type
  */
 const SingleFoodEvent = (props) => {
   return (
     <div className="SingleFoodEvent">
+      <p className="Card-title">{props.title}</p>
       <p className="Card-foodtype">{props.food_type}</p>
-      <p className="Card-photo">{props.photo}</p>
+      <p className="Card-photo">{props.photos}</p>
       <p className="Card-content">{props.content}</p>
     </div>
   );
