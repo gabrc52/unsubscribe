e9ec1e1c642de87c66473242494ad77bda122a09
Anna Kwon <annakwon@mit.edu>
2024-01-26 15:59:54 -0500

    changed food form

--- client/src/components/pages/FoodForm.tsx
+++ client/src/components/pages/FoodForm.tsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { NewFoodevent } from "../modules/NewPostInput";
-import FoodEvent from "../../../../shared/FoodEvent";
-import { get } from "../../utilities";
-
-const [foodEvents, setFoodEvents] = useState<FoodEvent[]>([]);
-const FoodForm = (props) => {
-  
-  useEffect(() => {
-    document.title = "Food Events";
-    get("/api/foodevents").then((foodEventObjs: FoodEvent[]) => {
-      let reversedFoodEventObjs = foodEventObjs.reverse();
-      setFoodEvents(reversedFoodEventObjs);
-    });
-  }, []);
-
-  const handleFoodEventSubmission = (foodEventObj) => {
-    setFoodEvents([foodEventObj].concat(foodEvents));
+import { post } from "../../utilities"; 
+const NewFoodeventPage = () => {
+  const addNewFoodevent = (foodevent) => {
+    post("/api/foodevent", foodevent).then(() => {});
   };
 
-  return <NewFoodevent onSubmit={handleFoodEventSubmission} />;
+  return (
+    <div>
+      <h1>Create a New Food Event</h1>
+      <NewFoodevent addNewFoodevent={addNewFoodevent} />
+    </div>
+  );
 };
 
-export { FoodForm, foodEvents };
+export default NewFoodeventPage;
