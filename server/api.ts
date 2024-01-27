239a2bbdf1b5a605d730e207b8a63542757a162d
Ari <ariapero@mit.edu>
2024-01-26 19:08:45 -0500

    added user endpoint

--- server/api.ts
+++ server/api.ts
@@ -43,6 +43,13 @@ router.get("/whoami", (req, res) => {
   }
   res.send(req.user);
 });
+
+router.get("/user", (req, res) => {
+  User.findById(req.query.userid).then((user) => {
+    res.send(user);
+  });
+});
+
 router.post("/initsocket", (req, res) => {
   // do nothing if user not logged in
   if (req.user) {
