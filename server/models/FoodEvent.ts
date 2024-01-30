c4c5e35ae23cb9346f119108ab12c7337c211bc4
Gabriel Rodríguez <rgabriel@mit.edu>
2024-01-30 00:26:35 -0500

    update food event schema

--- server/models/FoodEvent.ts
+++ server/models/FoodEvent.ts
@@ -1,14 +1,17 @@
 import { Schema, model, Document } from "mongoose";
 
-type FoodCategory = "Meal" | "Snack" | "Drink" | "Groceries";
+export type FoodCategory = "Meal" | "Snack" | "Drink" | "Groceries";
 
 // we prefer to store the ID of the poster, when it was posted from the app directly
 // for posts imported from email, we use the available name (from the email)
 
 const FoodEventSchema = new Schema({
+  // Set when posting from the app
   creator_userId: String,
+  // Set when posting from email
   emailer_address: String,
   emailer_name: String,
+
   food_category: String,
   food_type: String, // Cuisine or dish type (pizza, sandwiches, etc.)
   scheduled: Boolean, // true if a future scheduled event, false if available now
@@ -16,25 +19,30 @@ const FoodEventSchema = new Schema({
   photos: [String],
   title: String, // for email subject //mapped to food type
   content: String, // description / email body
+  // date that the food event was posted
   postedDate: {
     type: Date,
+    // automatically set to now so no need to do anything else :D
     default: Date.now,
   },
+  // date the food event is scheduled to (if scheduled is true)
+  scheduledDate: Date,
 });
 
 export interface FoodEvent extends Document {
-  creator_userId: string;
-  emailer_address: string;
-  emailer_name: string;
-  food_category: FoodCategory;
-  food_type: string;
-  location: String;
+  creator_userId?: string;
+  emailer_address?: string;
+  emailer_name?: string;
+  food_category?: FoodCategory;
+  food_type?: string;
+  location?: String;
   scheduled: boolean;
   photos: string[];
-  title: string;
+  title?: string;
   content: string;
   _id: string;
   postedDate: Date;
+  scheduledDate: Date;
 }
 
 const FoodEventModel = model<FoodEvent>("FoodEvent", FoodEventSchema);
