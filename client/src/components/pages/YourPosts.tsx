470dd69ae2472aaec2b5d59b07488a98b8b58e70
Ari <ariapero@mit.edu>
2024-01-30 09:35:57 -0500

    YourPosts personal message generator

--- client/src/components/pages/YourPosts.tsx
+++ client/src/components/pages/YourPosts.tsx
@@ -39,6 +39,7 @@ const YourPosts = (props: {}) => {
   //     return <img className="YourPosts-avatar" src={tim} />;
   //   }
   // };
+
   const renderAvatar = () => {
     // Handle Google avatars
     if (user.picture) {
@@ -53,11 +54,59 @@ const YourPosts = (props: {}) => {
     }
   };
 
+  const getRandomPersonalMessage = (tier) => {
+    const messages = {
+      lessThan5: [
+        "You're a food waste warrior in the making! Keep those posts coming ;)",
+        "Every post means one less 'unsubscribe pls🥺' making it to free-foods xD (claims unverified...)"
+      ],
+      between5And9: [
+        "Thanks for being a food waste warrior! Every post makes a difference :)",
+        "You're a super poster! Your foodie adventures are making waves.",
+      ],
+      between10And24: [
+        "You're a free food fiend! Your posts are spreading true culinary joy :P",
+        "Feaster of freebies! Poster of the delicious!",
+      ],
+      between25And49: [
+        "You're a foodie legend! Your posts have reached legendary status.",
+        "Legendary muncher! Your posts are the stuff of food folklore.",
+      ],
+      fiftyOrMore: [
+        "You're a true Unsubscribe champion! Your posts have set the bar high.",
+        "Champion of munching! Your posts are a *feast* for the eyes.",
+      ],
+    };
+  
+    const randomIndex = Math.floor(Math.random() * 2); // Randomly choose between two messages
+    return messages[tier][randomIndex];
+  };
+  
+  const getPersonalMessage = () => {
+    const numberOfPosts = userPosts.length;
+
+    if (numberOfPosts < 5) {
+      return getRandomPersonalMessage("lessThan5");
+    } else if (numberOfPosts < 10) {
+      return getRandomPersonalMessage("between5And9");
+    } else if (numberOfPosts < 25) {
+      return getRandomPersonalMessage("between10And24");
+    } else if (numberOfPosts < 50) {
+      return getRandomPersonalMessage("between25And49");
+    } else {
+      return getRandomPersonalMessage("fiftyOrMore");
+    }
+  };
+
   return (
     <>
       <div className="YourPosts-container">
         <div>
-        <h1 className="YourPosts-name u-flex u-flex-alignCenter">{renderAvatar()} &nbsp;{user.name}</h1></div>
+        <h1 className="YourPosts-name u-flex u-flex-alignCenter">{renderAvatar()} &nbsp;{user.name}</h1>
+        <p className="YourPosts-congratulatory-message u-flex u-flex-alignCenter">
+        {getPersonalMessage()}
+        </p>
+        </div>
         <hr className="YourPosts-linejj" />
         <div className="YourPosts-feed u-flex">
           {userPosts.map((post) => (
