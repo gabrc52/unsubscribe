e5b3ae32c3fd72bc1b63b4163f70f3cb33640c33
Gabriel Rodríguez <rgabriel@mit.edu>
2024-01-31 06:34:57 -0500

    whops food type not category

--- client/src/components/modules/OptionsButton.tsx
+++ client/src/components/modules/OptionsButton.tsx
@@ -53,7 +53,7 @@ export default function OptionsButton(foodEvent: FoodEvent) {
       });
     } else {
       // In practice, the title isn't actually rendered (at least on Android)
-      const title = foodEvent.title ?? `${foodEvent.food_category} in ${foodEvent.location}`;
+      const title = foodEvent.title ?? `${foodEvent.food_type} in ${foodEvent.location}`;
       // Mobile
       navigator.share({
         title: title,
