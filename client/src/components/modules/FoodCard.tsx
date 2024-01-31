09ccd7e2a2860da8745fc4d8c87df5aa110016e8
Gabriel Rodríguez <rgabriel@mit.edu>
2024-01-31 04:15:42 -0500

    Revert "food card comments button ripple fix yippeegit add .git add ."
    This reverts commit 9ec971d5fd388afbc58fe448e19f66849a9d53f0.

--- client/src/components/modules/FoodCard.tsx
+++ client/src/components/modules/FoodCard.tsx
@@ -73,7 +73,6 @@ const FoodCard = (foodEvent: FoodEvent) => {
   }, []);
 
   const handleMarkAsGone = () => {
-    setMarkedGone(!markedGone);
     if (!markedGone) {
       get("/api/whoami").then((user) => {
         if (user.name) {
@@ -87,7 +86,8 @@ const FoodCard = (foodEvent: FoodEvent) => {
         `Are you sure you want to mark this as gone? "${foodEvent.title || foodEvent.food_type}"`
       );
       if (reallyMarkAsGone) {
-        fetch("/foodevents/markAsGone", {
+        setMarkedGone(!markedGone);
+        fetch(`/api/foodevents/markAsGone/${foodEvent._id}`, {
           method: "POST",
         }).catch(console.error);
       }
@@ -169,13 +169,13 @@ const FoodCard = (foodEvent: FoodEvent) => {
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton> */}
-        {!markedGone && (
+        {!foodEvent.isGone && (
           <Button variant="contained" onClick={handleMarkAsGone}>
             Mark as gone?
           </Button>
         )}
-        {markedGone && foodEvent.isGone && (
-          <Typography variant="body2">Marked gone by {markedGoneBy}</Typography>
+        {foodEvent.isGone && (
+          <Typography variant="body2">Marked gone by {foodEvent.markedGoneName}</Typography>
         )}
         <ExpandMore
           expand={expanded}
@@ -183,11 +183,8 @@ const FoodCard = (foodEvent: FoodEvent) => {
           aria-expanded={expanded}
           aria-label="show/hide comments"
           sx={{
-            // DO NOT CHANGE THESE!!!!!!!!
+            paddingBottom: 0,
             paddingRight: 0,
-            paddingBottom: expanded ? 1 : 0,
-            paddingTop: expanded ? 0 : "auto",
-            paddingLeft: expanded ? 1 : "auto",
             borderRadius: "16px 16px 16px 16px",
           }}
         >
